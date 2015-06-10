f905cb560bb102032614cbd30e0641c1184e62e2
mayankiitg <gmayank43@gmail.com>
2015-06-10 18:54:16 +0530

    Create settings.py

---
+++ Twitter_Crawler/Rest-API/settings.py
@@ -0,0 +1,7 @@
+#put your application key and tokens provided to you from twitter after registering your application.
+consumer_key=""
+consumer_secret= ""
+access_token=""
+access_token_secret= ""
+
+database = 'twitterutils'
